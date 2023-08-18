f9db8a1df21148caf9aafac34e5a056bbc9520d8
Yasir558 <110908280+Yasir558@users.noreply.github.com>
2023-08-18 17:38:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+ const text = document.qurreySelector(".sec-text");
+
+ const textLoad = () => {
+     setTimeout(() => {
+ text.textcontent ="YASIR ALI";
+
+     }, 0);
+
+     setTimeout(() => {
+        text.textcontent ="ARSHAD ALI";
+       
+            }, 4000);
+
+
+
+
+ }
+
+ textLoad();
+
+
